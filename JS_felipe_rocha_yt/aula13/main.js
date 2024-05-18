c14fc22f35e2918b6860a4f69d58ccfe2a22ca85
JoaoVictor-Noschang <joaovictornoschangsilva601@gmail.com>
2024-05-17 20:46:05 -0400

    add aula 13
    Selecionando e manipulando elementos pelo DOM

---
+++ JS_felipe_rocha_yt/aula13/main.js
@@ -0,0 +1,14 @@
+// Manipulando os elementos do DOM
+const items = document.querySelector(".items");
+const button = document.querySelector('.btn');
+
+//items.remove();
+
+//items.firstElementChild.remove();
+//items.lastElementChild.remove();
+
+//items.children[1].textContent = 'Item Um';
+
+//items.lastElementChild.innerHTML = '<h1>Hello world!</h>';
+
+button.style.background = 'red';
